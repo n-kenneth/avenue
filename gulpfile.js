711569f7a73d2e5bac77a255ab171388d885b842
Niel Kenneth <nkenneth19@gmail.com>
2018-05-08 16:08:02 +0800

    Inital commit

---
+++ gulpfile.js
@@ -0,0 +1,41 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const browserSync = require('browser-sync').create();
+const useref = require('gulp-useref');
+const uglify = require('gulp-uglify');
+const gulpIf = require('gulp-if');
+const plumber = require('gulp-plumber');
+const autoprefixer = require('gulp-autoprefixer');
+const notify = require('gulp-notify');
+
+gulp.task('sass', () => {
+  return gulp.src('./src/scss/**/*.scss')
+    .pipe(sass())
+    .pipe(autoprefixer('last 2 versions'))
+    .pipe(gulp.dest('./src/css/'))
+    .pipe(plumber({
+      errorHandler(err) {
+        notify.onError({
+          title: 'Gulp error in ' + err.plugin,
+          message: err.toString()
+        })(err);
+      }
+    }))
+    .pipe(browserSync.stream());
+});
+
+gulp.task('serve', ['sass'], () => {
+
+  browserSync.init({
+    server: {
+      baseDir: './src'
+    },
+  });
+
+  gulp.watch( './src/scss/**/*.scss', ['sass'] );
+  gulp.watch('./src/**/*.html', browserSync.reload);
+  gulp.watch('./src/**/*.js', browserSync.reload);
+
+});
+
+gulp.task('default', ['serve']);
